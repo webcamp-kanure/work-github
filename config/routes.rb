a244876c22bd8c96e5db3d9bc7a9f6ffbb253dc3
EC2 Default User <ec2-user@ip-172-31-26-40.ap-northeast-1.compute.internal>
2024-04-15 09:23:21 +0000

    route fixed

--- config/routes.rb
+++ config/routes.rb
@@ -6,25 +6,26 @@ Rails.application.routes.draw do
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  root to: "homes#top"
-  get 'about' => 'homes#about'
-  resources :items, only: [:index,:show]
-  
-  get 'customers/my_page' => 'customers#show'
-  get 'customers/information/edit' => 'customers#edit'
-  patch 'customers/information' => 'customers#update'
-  get 'customers/unsubscribe' => 'customers/unsubscribe'
-  patch 'customers/withdraw' => 'customers/withdraw'
-  
-  resources :cart_items, only: [:index,:update,:destroy,:create]
-  delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
-  
-  resources :orders, only: [:new,:create,:index,:show]
-  post 'orders/confirm' => 'orders#confirm'
-  get 'orders/thanks' => 'orders#thanks'
-  
-  resources :addresses, only: [:index,:edit,:create,:update,:destroy]
-  
+  scope module: :public do
+    root to: "homes#top"
+    get 'about' => 'homes#about'
+    resources :items, only: [:index,:show]
+    
+    get 'customers/my_page' => 'customers#show'
+    get 'customers/information/edit' => 'customers#edit'
+    patch 'customers/information' => 'customers#update'
+    get 'customers/unsubscribe' => 'customers#unsubscribe'
+    patch 'customers/withdraw' => 'customers#withdraw'
+    
+    resources :cart_items, only: [:index,:update,:destroy,:create]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
+    
+    resources :orders, only: [:new,:create,:index,:show]
+    post 'orders/confirm' => 'orders#confirm'
+    get 'orders/thanks' => 'orders#thanks'
+    
+    resources :addresses, only: [:index,:edit,:create,:update,:destroy]
+  end
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
