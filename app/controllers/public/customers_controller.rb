0865488a5629cd5fbec8f2023abc29fc754c6a88
touchtak <159424108+touchtak@users.noreply.github.com>
2024-04-15 09:04:03 +0000

    add current_cart_item method in application_controller, add action_details in public/homes items customers cart_items

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,18 +2,38 @@ class CustomersController < ApplicationController
   before_action :authenticate_customer!
 
   def show
+    @customer = current_customer
   end
 
   def edit
+    @customer = current_customer
   end
 
   def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+      flash[:notice] = "会員情報を変更しました。"
+      render :show
+    else
+      render :edit
+    end
   end
 
   def unsubscribe
+    @customer = current_customer
   end
 
   def withdraw
+    @customer = current_customer
+    @customer.update(is_active: false)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :postal_code, :address, :telephone_number)
   end
 
 end
