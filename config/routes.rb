ac90466b0e6c4ecc195503d37d0122fa523d0a05
touchtak <159424108+touchtak@users.noreply.github.com>
2024-04-15 09:34:28 +0000

    add end

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,36 @@
 Rails.application.routes.draw do
-  
+
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  root to: "homes#top"
+  root to: "public/homes#top"
   get 'about' => 'homes#about'
   resources :items, only: [:index,:show]
-  
+
   get 'customers/my_page' => 'customers#show'
   get 'customers/information/edit' => 'customers#edit'
   patch 'customers/information' => 'customers#update'
   get 'customers/unsubscribe' => 'customers/unsubscribe'
   patch 'customers/withdraw' => 'customers/withdraw'
-  
+
   resources :cart_items, only: [:index,:update,:destroy,:create]
   delete 'cart_items/destroy_all' => 'cart_items#destroy_all'
-  
+
   resources :orders, only: [:new,:create,:index,:show]
   post 'orders/confirm' => 'orders#confirm'
   get 'orders/thanks' => 'orders#thanks'
-  
+
   resources :addresses, only: [:index,:edit,:create,:update,:destroy]
-  
+
   # 管理者用
   # URL /admin/sign_in ...
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
   }
-  
+
   namespace :admin do
     root to: "homes#top"
     resources :items, only: [:index,:new,:create,:show,:edit,:update]
@@ -39,7 +39,7 @@ Rails.application.routes.draw do
     resources :orders, only: [:show,:update]
     resources :order_details, only: [:update]
   end
-  
+
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
