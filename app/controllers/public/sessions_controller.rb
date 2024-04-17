baaf3696d32a77b2fedaac8a798e52661f1c3915
touchtak <159424108+touchtak@users.noreply.github.com>
2024-04-17 03:12:29 +0000

    added Genres

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -34,17 +34,18 @@ class Public::SessionsController < Devise::SessionsController
   # アクティブであるかを判断するメソッド
   def customer_state
     # 【処理内容1】 入力されたemailからアカウントを1件取得
-    customer = Customer.find_by(email: params[:customer][:email])
+    customer = Customer.find_by(email: params[:email])
     # 【処理内容2】 アカウントを取得できなかった場合、このメソッドを終了する
     return if customer.nil?
     # 【処理内容3】 取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了する
-    return unless customer.valid_password?(params[:customer][:password])
-  
+    return unless customer.valid_password?(params[:password])
     # 【処理内容4】 アクティブでない会員に対する処理
     return if customer.is_active == true
-  
-    #if customer.is_active == false
-    redirect_to sign_in_path
+
+    if customer.is_active == false
+      redirect_to sign_in_path
+    end
+
   end
 
 end
