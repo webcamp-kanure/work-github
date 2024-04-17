9a624a7e6c0fa26a7f7e1f4d07371190a59d46b3
touchtak <159424108+touchtak@users.noreply.github.com>
2024-04-17 04:00:20 +0000

    fixed log in funtion,changed path in _header.html.erb

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -5,40 +5,41 @@ class Public::SessionsController < Devise::SessionsController
   before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
-  def new
-  #   super
-  end
+  # def new
+  #   self.resource = resource_class.new(sign_in_params)
+  # #   super
+  # end
 
-  # POST /resource/sign_in
-  def create
-  #   super
-  end
+  # # POST /resource/sign_in
+  # def create
+  # #   super
+  # end
 
-  # DELETE /resource/sign_out
-  def destroy
-  #   super
-  end
+  # # DELETE /resource/sign_out
+  # def destroy
+  # #   super
+  # end
 
-  def after_sign_in_path_for(resource)
+  # def after_sign_in_path_for(resource)
 
-  end
+  # end
 
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_in_params
-    devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :encrypted_password) }
-  end
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in) { |u| u.permit(:email, :encrypted_password) }
+  # end
 
   private
   # アクティブであるかを判断するメソッド
   def customer_state
     # 【処理内容1】 入力されたemailからアカウントを1件取得
-    customer = Customer.find_by(email: params[:email])
+    customer = Customer.find_by(email: params[:customer][:email])
     # 【処理内容2】 アカウントを取得できなかった場合、このメソッドを終了する
     return if customer.nil?
     # 【処理内容3】 取得したアカウントのパスワードと入力されたパスワードが一致していない場合、このメソッドを終了する
-    return unless customer.valid_password?(params[:password])
+    return unless customer.valid_password?(params[:customer][:password])
     # 【処理内容4】 アクティブでない会員に対する処理
     return if customer.is_active == true
 
