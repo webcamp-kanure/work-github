69f799dfa64022fa7d19366a425163688ae93d8a
EC2 Default User <ec2-user@ip-172-31-26-40.ap-northeast-1.compute.internal>
2024-04-19 04:32:04 +0000

    create _left_sidebar

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -2,11 +2,13 @@ class Public::ItemsController < ApplicationController
 
   def index
     @items = Item.all
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item_new = CartItem.new
+    @genres = Genre.all
   end
 
 
