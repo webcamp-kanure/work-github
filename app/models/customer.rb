560500ec2a22a7867d7eb16bc18701da3ae21d45
hrsmsnr <158572407+hrsmsnr@users.noreply.github.com>
2024-04-19 12:34:47 +0900

    Update customer.rb
    インデントを忘れてました

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,14 +10,14 @@ class Customer < ApplicationRecord
   has_many :cart_items, dependent: :destroy
 
   with_options presence: true do
-  validates :last_name
-  validates :first_name
-  validates :last_name_kana
-  validates :first_name_kana
-  validates :postal_code
-  validates :address
-  validates :telephone_number
-  validates :email
-  validates :password
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :postal_code
+    validates :address
+    validates :telephone_number
+    validates :email
+    validates :password
   end
 end
