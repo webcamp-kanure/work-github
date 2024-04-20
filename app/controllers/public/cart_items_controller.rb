edd472ca1a3bbb0fdba0ae23f91a1b17cd47fbd1
touchtak <159424108+touchtak@users.noreply.github.com>
2024-04-20 04:18:22 +0000

    fixed orders system

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,7 +2,7 @@ class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
 
   def index
-    @cart_item = current_customer.cart_items.all
+    @cart_items = current_customer.cart_items.all
   end
 
   def create
