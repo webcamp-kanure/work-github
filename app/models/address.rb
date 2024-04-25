e3a553e175c840311c7581946ac7039b040116e7
EC2 Default User <ec2-user@ip-172-31-26-40.ap-northeast-1.compute.internal>
2024-04-25 03:57:13 +0000

    add order.status

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,5 +3,4 @@ class Address < ApplicationRecord
   belongs_to :customer
 
   validates :postal_code, length: { is: 7 }, numericality: { only_integer: true }
-  validates :telephone_number, length: { in: 10..11 }, numericality: { only_integer: true }
 end
